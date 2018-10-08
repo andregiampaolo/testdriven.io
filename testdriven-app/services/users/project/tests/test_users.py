443d83d471a36f9adbab9a3cf0ebd6c4c22fba17
André Giampaolo <andregiampaolo@gmail.com>
2018-10-08 09:13:11 -0300

    Adding test json object is empty

--- testdriven-app/services/users/project/tests/test_users.py
+++ testdriven-app/services/users/project/tests/test_users.py
@@ -30,6 +30,20 @@ class TestUserService(BaseTestCase):
             self.assertEqual(response.status_code,201)
             self.assertIn('michael@mherman.org was added!', data['message'])
             self.assertIn('success', data['status'])
+    
+    def test_invalid_json(self):
+        """Ensure error is thrown if the JSON object is empty."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({}),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code,400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
 
 if __name__ == '__main__':
     unittest.main()
