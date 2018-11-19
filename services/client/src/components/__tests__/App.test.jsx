f304b48442f6d0d22fc44adcca7d93532f32ac34
André Giampaolo <andregiampaolo@gmail.com>
2018-11-19 21:03:10 -0200

    Check App render is working

---
+++ services/client/src/components/__tests__/App.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../App';
+
+text('App renders without crashing', () => {
+    const wrapper = shallow(<App/>);
+});
