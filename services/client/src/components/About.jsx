ea0fe4c92498c34c8dce015a5cb696f785b6bbce
André Giampaolo <andregiampaolo@gmail.com>
2018-11-19 21:13:47 -0200

    Create About component

---
+++ services/client/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react-dom';
+
+const About = () => (
+    <div>
+        <h1 className="title ls-1">About</h1>
+        <hr/><br/>
+        <p>Add something relevant here.</p>
+    </div>
+)
+
+export default About;
