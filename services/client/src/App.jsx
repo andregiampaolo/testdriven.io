375fdeec2dae249315987129de5967fa0fae6d2c
André Giampaolo <andregiampaolo@gmail.com>
2018-11-21 00:55:41 -0200

    Add switch component for change view by router

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, Switch } from 'react-router-dom';
 
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
@@ -54,19 +55,24 @@ class App extends Component{
                 <div className="container">
                     <div className="columns">
                         <div className="column is-half">
-                            <br/>
-                            <h1 className="title is-1">All Users</h1>
-                            <hr/><br/>
-                            <AddUser
-                                username={this.state.username}
-                                email={this.state.email}
-                                addUser={this.addUser}
-                                handleChange={this.handleChange}
-                            />
-                            <br/><br/>
-                            <UsersList users={this.state.users}/>
-                            <br/>
-                            <About/>
+                        <br/>
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                    <div>
+                                        <h1 className="title is-1">All Users</h1>
+                                        <hr/><br/>
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            addUser={this.addUser}
+                                            handleChange={this.handleChange}
+                                        />
+                                        <br/><br/>
+                                        <UsersList users={this.state.users}/>
+                                    </div>
+                                )}/>
+                                <Route exact path='/about' component={About} />
+                            </Switch>
                         </div>
                     </div>
                 </div>
