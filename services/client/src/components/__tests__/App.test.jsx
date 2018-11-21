350768d78f5a699cd51c9b0946b71680725ba1c5
André Giampaolo <andregiampaolo@gmail.com>
2018-11-21 19:35:05 -0200

    Fix the name of the function

--- services/client/src/components/__tests__/App.test.jsx
+++ services/client/src/components/__tests__/App.test.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../../App';
 
-text('App renders without crashing', () => {
+test('App renders without crashing', () => {
     const wrapper = shallow(<App/>);
 });
