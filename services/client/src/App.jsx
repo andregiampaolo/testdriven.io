53a69a8d966927bbb90269d1e6bce524db4e18f8
André Giampaolo <andregiampaolo@gmail.com>
2018-11-21 22:38:31 -0200

    Add form component to application

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -6,6 +6,7 @@ import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
 import NavBar from './components/NavBar';
+import Form from './components/Form';
 
 class App extends Component{
     constructor(){
@@ -15,6 +16,11 @@ class App extends Component{
             username: '',
             email: '',
             title: 'TestDriven.io',
+            formData: {
+                username: '',
+                email: '',
+                password: ''
+            }
         };
         this.addUser = this.addUser.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -61,6 +67,18 @@ class App extends Component{
                             <div className="column is-half">
                             <br/>
                                 <Switch>
+                                    <Route exact path='/register' render={() => (
+                                        <Form
+                                            formType={'Register'}
+                                            formData={this.state.formData}
+                                        />
+                                    )} />
+                                    <Route exact path='/login' render={() => (
+                                        <Form
+                                            formType={'Login'}
+                                            formData={this.state.formData}
+                                        />
+                                    )} />
                                     <Route exact path='/' render={() => (
                                         <div>
                                             <h1 className="title is-1">All Users</h1>
